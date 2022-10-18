8fb1b47ce3241cbffcc6e72088a643decb3e150b
MohammedElhajj <m93osman@yahoo.com>
2022-10-17 17:21:50 -0700

    Task 11. Prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,9 +11,26 @@ int main(void)
 	int b;
 	int c;
 
-	for (a =
-
-
-
-
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <= 9; c++)
+			{
+				if (a != b && b != c && a != c && c > b && b > a)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+					if (!(a == 7 && b == 8 && c == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
