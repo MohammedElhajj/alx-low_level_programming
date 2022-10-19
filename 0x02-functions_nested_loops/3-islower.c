7923bf050532bbd01b34d59a9aeb5e000a951892
MohammedElhajj <m93osman@yahoo.com>
2022-10-18 17:05:27 -0700

    Task 3. Checks for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _islower - Check if a char is lowercase
- * @cc: is the character to be checked as int in ASCII code
+ * _islower - Checks if a char is lowercase
+ * @c: The character to be checked in ASCII code
  *
  * Return: 1 if char is lowercase character, otherwise 0.
  */
