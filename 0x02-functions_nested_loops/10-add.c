39d077bce183d8bfd84bee59257339b223ff753b
MohammedElhajj <m93osman@yahoo.com>
2022-10-19 05:05:31 -0700

    Task 10. Function adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @m: The first integeer number
+ * @n: The second integer number
+ *
+ * Return: The result of the addition
+ */
+int add(int m, int n)
+{
+	return (m + n);
+}
