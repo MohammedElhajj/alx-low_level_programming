57d13b3e73730bbc9d89befd4bd5e6eeda1ca484
MohammedElhajj <m93osman@yahoo.com>
2022-10-20 14:54:54 -0700

    Task 5. Function prints 10 times the numbers from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,7 +12,11 @@ void more_numbers(void)
 	for (count = 1; count <= 10; count++)
 	{
 		for (n = 0; n <= 14; n++)
-			_putchar(n + 48);
+		{
+			if (n >= 10)
+				_putchar((n / 10) + '0')
+			_putchar((n % 10) + '0');
+		}
 		_putchar('\n');
 	}
 }
