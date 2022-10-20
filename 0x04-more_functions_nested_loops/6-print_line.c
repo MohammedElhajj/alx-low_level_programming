f28701289a9e85e3b82fc75b56ee8e6aea82ca90
MohammedElhajj <m93osman@yahoo.com>
2022-10-20 16:26:38 -0700

    Task 6. Function draws a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - draw  a straight line in the terminal
+ * @n: the number of times the character _ should be printed
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n);
+	}
+}
