7c67e157b0df31d85255063c8edb6a81dfa060f6
MohammedElhajj <m93osman@yahoo.com>
2022-10-23 09:04:58 -0700

    Task 12. Prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,9 +7,27 @@
  */
 int main(void)
 {
-	int i;
-	int j;
+	int i, j; /* i for ones and j for tens */
 
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j) /* ones != tens and ones > tens */
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 	putchar('\n');
 	return (0);
 }
