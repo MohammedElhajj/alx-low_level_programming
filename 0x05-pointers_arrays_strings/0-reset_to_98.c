c6e6488258fda076776b679618c9b135e8f1d7a7
MohammedElhajj <m93osman@yahoo.com>
2022-10-25 12:48:29 -0700

    Task 0. Reset the value of an int variable to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - take a pointer to an int as paramete
+ * and updates the value it points to to 98
+ * @n:the int variable
+ * Return: n after reset it to 98
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
