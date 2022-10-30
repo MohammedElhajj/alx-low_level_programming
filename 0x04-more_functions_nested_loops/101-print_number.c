5205a7b24cb5f27d083a91f75117215500058fda
MohammedElhajj <m93osman@yahoo.com>
2022-10-30 03:22:44 -0700

    Task 12. Print an integer number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,14 +3,9 @@
 /**
  * print_number - print an integer number
  * @n: the int number to be printed
- *
- * Return: the int number n.
  */
 void print_number(int n)
 {
-	if (n == 0)
-		_putchar('0');
-
 	if (n < 0)
 	{
 		_putchar('-');
