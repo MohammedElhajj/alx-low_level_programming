fa9d5651945599b0f26609dec3ba9a482cb008c5
MohammedElhajj <m93osman@yahoo.com>
2022-10-30 06:31:14 -0700

    Task 10. Convert a string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to be printed
+ * Return: string as a number
+ */
+int _atoi(char *s)
+{
+	int nb = 0;
+	int sign = -1;
+	int i = 0; /* i:index */
+
+	if (s[0] == '-') /* update sign for negative numbers */
+	{
+		sign = -1;
+		i++;
+	}
+
+	for (; s[i] != '\0'; i++)
+		nb = nb * 10 + (s[i] - '0');
+
+	return (sign * nb);
+}
