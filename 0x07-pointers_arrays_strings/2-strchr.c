e463e7bf9f792c8e96ca4c6ad6e09ecdfa6a6e3a
MohammedElhajj <m93osman@yahoo.com>
2022-11-01 15:31:29 -0700

    Task 2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	if (s == NULL)
-		return (NULL);
 	while (*s != '\0')
 	{
 		if (*s == c)
 			return (s);
 		s++;
 	}
+	if (*s == c)
+		return (s);
 	return (NULL);
 }
