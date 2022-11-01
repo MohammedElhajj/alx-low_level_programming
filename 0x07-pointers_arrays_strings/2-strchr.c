9061e5827e4e602ed5fb5f5b917980eb9ceab607
MohammedElhajj <m93osman@yahoo.com>
2022-11-01 15:04:56 -0700

    Task 2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - locates a character in a string
@@ -18,5 +19,5 @@ char *_strchr(char *s, char c)
 			return (s_char);
 		s++;
 	}
-	return ('NULL');
+	return (NULL);
 }
