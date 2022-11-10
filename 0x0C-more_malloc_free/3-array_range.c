250abfbe4565ebb4900da16cc4d9418555e85b96
MohammedElhajj <m93osman@yahoo.com>
2022-11-10 14:31:40 -0800

    Task 3. Function creates an array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * array_range -  creates an array of integers
@@ -16,10 +17,10 @@ int *array_range(int min, int max)
 
 	/* Return NULL if min is grater than max or malloc fails */
 	if (min > max)
-		return (NULL);
+		return (0);
 	a = malloc((max - min + 1) * sizeof(int));
-	if (a == NULL)
-		return (NULL);
+	if (a == 0)
+		return (0);
 
 	/* Loop to create the array */
 	i = 0;
