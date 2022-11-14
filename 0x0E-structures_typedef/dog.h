6b2d1905205cbb0350b0358b653153d91f77c956
MohammedElhajj <m93osman@yahoo.com>
2022-11-14 13:14:47 -0800

    Task 0. Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - define a new type struct dog
+ * for describing a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
