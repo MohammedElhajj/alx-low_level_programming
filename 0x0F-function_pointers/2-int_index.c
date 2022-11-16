f24dc23093d67a3ffe8ae786c9fbbcbf99e36a2b
MohammedElhajj <m93osman@yahoo.com>
2022-11-16 13:29:40 -0800

    Task 2. Function searches for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * int_index - searches for an integer
@@ -13,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array[i] && cmp(array[i]))
+	if (array && cmp)
 	{
 		for (i = 0; i < size; i++)
 		{
