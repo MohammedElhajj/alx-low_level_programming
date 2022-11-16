8b0a9d2ece8f4935d126e2a6334057f029e6aa71
MohammedElhajj <m93osman@yahoo.com>
2022-11-16 13:21:11 -0800

    Task 2. Function searches for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: the array to search in for the integer
+ * @size: the size of the array
+ * @cmp: a pointer to the comparison function
+ *
+ * Return: The index of the first element for which the cmp
+ * function does not return 0 otherwise -1.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if (array[i] && cmp(array[i]))
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1); /* No element matches or negative size */
+}
