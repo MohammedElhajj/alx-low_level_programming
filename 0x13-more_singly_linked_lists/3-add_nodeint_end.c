d9f366cedc254efa97b26cb515d706ba04850aa2
MohammedElhajj <m93osman@yahoo.com>
2022-11-29 03:49:29 -0800

    Tasks 0-4. More singly linked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,16 +21,16 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new->n = n;
 
 	new->next = NULL; /* Point to NULL */
-	temp = *head;
 
-	if (temp == NULL)
+	if (*head == NULL)
 		*head = new;
 	else
 	{
+		temp = *head;
 		while (temp->next != NULL)
 			temp = temp->next;
 		temp->next = new;
 	}
 
-	return (temp);
+	return (*head);
 }
