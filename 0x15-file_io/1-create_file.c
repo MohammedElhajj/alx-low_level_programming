20132d585bfa2b89e5cb666e58fa0d718a5e6efc
MohammedElhajj <m93osman@yahoo.com>
2022-12-05 07:16:55 -0800

    Function to create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,13 +18,10 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (text_len = 0; text_content[text_len]; text_len++)
-			;
-	}
-	else
+	if (!text_content)
 		text_content = "";
+	for (text_len = 0; text_content[text_len]; text_len++)
+		;
 
 	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	w = write(o, text_content, text_len);
