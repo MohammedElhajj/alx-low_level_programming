53c6546adf67d7f2e9d73b3268c7d08969342c3d
MohammedElhajj <m93osman@yahoo.com>
2022-12-26 02:12:23 -0800

    Task 11. A program that generates random valid passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords
+ *
+ * Return: Always 0. (Success)
+ */
+int main(void)
+{
+	/* Initialize counter */
+	int passwd, i;
+
+	/**
+	 * Seed the random password generator with
+	 * current time to create different values
+	 */
+	srand(time(NULL));
+
+	i = 0;
+	while (i <= 264)
+	{
+		passwd = (rand() % 128);
+		i += passwd;
+	}
+
+	printf("%c", 2772 - i);
+	return (0);
+}
